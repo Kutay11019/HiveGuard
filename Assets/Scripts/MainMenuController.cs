ab7218da53a80192d3dc3acf4840cba0dd742187
Kutay11019 <118713769+Kutay11019@users.noreply.github.com>
2026-05-12 01:50:45 +0300

    Improve main menu UI for mobile

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -17,29 +17,49 @@ public class MainMenuController : MonoBehaviour
 
     public void StartGame()
     {
+        if (string.IsNullOrWhiteSpace(gameplaySceneName))
+        {
+            Debug.LogError("Gameplay scene name is empty! Please set it in the Inspector.");
+            return;
+        }
+
         SceneManager.LoadScene(gameplaySceneName);
     }
 
     public void ShowHowToPlay()
     {
-        mainMenuPanel.SetActive(false);
-        howToPlayPanel.SetActive(true);
+        if (mainMenuPanel != null)
+        {
+            mainMenuPanel.SetActive(false);
+        }
+
+        if (howToPlayPanel != null)
+        {
+            howToPlayPanel.SetActive(true);
+        }
     }
 
     public void ShowMainMenu()
     {
-        mainMenuPanel.SetActive(true);
-        howToPlayPanel.SetActive(false);
+        if (mainMenuPanel != null)
+        {
+            mainMenuPanel.SetActive(true);
+        }
+
+        if (howToPlayPanel != null)
+        {
+            howToPlayPanel.SetActive(false);
+        }
     }
 
     public void ExitGame()
     {
         Debug.Log("Exit button pressed.");
 
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #else
+#else
         Application.Quit();
-        #endif
+#endif
     }
 }
